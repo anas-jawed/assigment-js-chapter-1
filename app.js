da2b2b0e824064a76944c8137bfd17b1f4de67d2
Anas Jawed <anasjawedbmj@gmail.com>
2024-10-14 11:26:53 +0300

    Chapter1JavaScript

---
+++ app.js
@@ -0,0 +1,34 @@
+// 1 . // Write a script to greet your website visitor using JS alert 
+// // box
+
+// let greet  = prompt("");
+
+// 2 . Write a scri[t to display following message on your web page:
+
+// let greet = prompt("Error! Please enter a valid password");
+
+// console.log(greet);
+
+// 3. Write a script to display following message on your web 
+// page: (Hint : Use line break)
+
+// let message = prompt("Welcome to JS Land", "</br> happy Coding!");
+
+// console.log(message);
+
+// 4. Write a script to display following messages in sequence
+
+// let msg = prompt("Welcome to JS Land...");
+// console.log("msg");
+
+// let msg1 = alert("Happy Coding!, <br> prevent this page from crating additional dialogs.");
+
+// console.log(msg1);
+
+
+// 5.  Generate the following message through browser’s 
+// developer console:
+
+// let msg = alert("hello i can run JS through my web brwoser's console")
+
+// console.log(msg);
